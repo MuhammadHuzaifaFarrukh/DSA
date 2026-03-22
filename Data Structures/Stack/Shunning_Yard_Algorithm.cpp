3995494b2a23075dce26072318b911881bbc9910
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2026-03-22 19:44:40 +0500

    Add files via upload

---
+++ Data Structures/Stack/Shunning_Yard_Algorithm.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+// Shunning Yard Algorithm is used to convert Infix to Postfix Expression using Stack.
+// Converting to postfix is easier to debug and evaulate an expression
+
+// Function to return precedence of operators
+int prec(char c)
+{
+    if (c == '^')
+        return 3;
+    if (c == '*' || c == '/')
+        return 2;
+    if (c == '+' || c == '-')
+        return 1;
+    return -1;      // If its not any operator like bracket or operands
+}
+
+string infixToPostfix(string s)
+{
+    stack<char> st;
+    string result = "";
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        char c = s[i];
+
+        // 1. If operand, add to result
+        if (isdigit(c))
+        {
+            result += c;
+        }
+        // 2. If '(', push to stack
+        else if (c == '(')
+        {
+            st.push('(');
+        }
+        // 3. If ')', pop until '('
+        else if (c == ')')
+        {
+            while (!st.empty() && st.top() != '(')
+            {
+                result += st.top();
+                st.pop();
+            }
+            st.pop(); // Remove '('
+        }
+        // 4. If operator
+        else
+        {
+            while (!st.empty() && prec(s[i]) <= prec(st.top()))
+            {
+                result += st.top();
+                st.pop();
+            }
+            st.push(c);
+        }
+    }
+
+    // 5. Pop remaining operators
+    while (!st.empty())
+    {
+        result += st.top();
+        st.pop();
+    }
+
+    return result;
+}
+
+int main()
+{
+    string exp = "a+b*(c^d-e)";
+    cout << "Postfix: " << infixToPostfix(exp) << endl;
+    return 0;
+}
