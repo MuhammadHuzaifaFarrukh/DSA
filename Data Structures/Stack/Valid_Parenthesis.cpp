01753972266d1005f706e3372810c0d3bf4c7f7c
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2026-03-22 19:43:15 +0500

    Add files via upload

---
+++ Data Structures/Stack/Valid_Parenthesis.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+bool isValid(string s)
+{
+    stack<char> st;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+        {
+            st.push(s[i]);
+        }
+        else
+        {
+            
+            if (st.empty())
+            {
+                return false;
+            }
+
+            char top = st.top();
+            if ((s[i] == ')' && top == '(') ||
+                (s[i] == '}' && top == '{') ||
+                (s[i] == ']' && top == '['))
+            {
+                st.pop(); // Matched!
+            }
+            else
+            {
+                return false; // Mismatch!
+            }
+        }
+    }
+    return st.empty();		// Stack is Emptied once all chars popped out , and if empty string case given then this gets handled as well.
+}
+
+int main()
+{
+    string test = "{[()]}";
+    if (isValid(test))
+    {
+        cout << "Valid " << endl;
+    }
+    else
+    {
+        cout << "Invalid " << endl;
+    }
+    return 0;
+}
