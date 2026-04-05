9ed5e7a66cc259d9f856670ca9f30a1cfdf92f9a
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2026-04-05 16:01:35 +0500

    Add files via upload

---
+++ Data Structures/Queue/Priority_Queue/Food_Ordering_System_VIP.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+struct Order
+{
+    string customer;
+    int priority; // higher means more urgent/VIP
+    bool operator<(const Order &other) const
+    {
+        return priority < other.priority;
+    }
+};
+int main()
+{
+    priority_queue<Order> pq;
+    int choice;
+    do
+    {
+        "\n";
+        cout << "\n--- Food Delivery System ---\n";
+        cout << "1. Add order\n";
+        cout << "2. Dispatch next order\n";
+        cout << "3. Show next order\n";
+        cout << "4. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+        cin.ignore();
+        switch (choice)
+        {
+        case 1:
+        {
+            Order o;
+            cout << "Enter customer name: ";
+            getline(cin, o.customer);
+            cout << "Enter priority (VIP/high value first): ";
+            cin >> o.priority;
+            cin.ignore();
+            pq.push(o);
+            break;
+        }
+        case 2:
+            if (pq.empty())
+                cout << "No orders pending.\n";
+            else
+            {
+                cout << "Dispatching order for " << pq.top().customer<<endl; 
+                pq.pop();
+            }
+            break;
+        case 3:
+            if (pq.empty())
+                cout << "No orders pending.\n";
+            else
+                cout << "Next order: " << pq.top().customer << "\n";
+            break;
+        case 4:
+            cout << "Exiting...\n";
+            break;
+        default:
+            cout << "Invalid choice.\n";
+        }
+    } while (choice != 4);
+    return 0;
+}
