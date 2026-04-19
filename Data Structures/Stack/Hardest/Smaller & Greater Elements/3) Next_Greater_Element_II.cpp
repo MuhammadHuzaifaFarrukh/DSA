be6062a7919979eec9be8df21ab77afe0dd96513
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2026-04-20 03:37:29 +0500

    Update 3) Next_Greater_Element_II.cpp

--- Data Structures/Stack/Hardest/Smaller & Greater Elements/3) Next_Greater_Element_II.cpp
+++ Data Structures/Stack/Hardest/Smaller & Greater Elements/3) Next_Greater_Element_II.cpp
@@ -5,12 +5,20 @@
 using namespace std;
 
 // This is another variant of next greater element.
-// We have to find the next greater element for each element , even if we can find for last element
+// We have to find the next greater element on right side for each element , even if we can find for last element
 // We have to treat our given array as circular array.
 
+// 1) Brute Force Method : O(n^2)
 
-// We treat nums1 and num2 = nums only
-// And Find the next greater for each element in the nums by treating it as circular array.
+// 2) Another Method is to use Stack and apply same logic which we did for all next greater / smaller elements but we will check in (nums + nums) 1D array that is of 2 times its length
+// Total elements 'n' so we check each element 2*n-1 times .
+// This takes O(n) Time Complexity and Space also O(n).
+// Others also used O(n) space as stack where we stored our indices.
+// But here even more memory , like double sized array is being created.
+// So Space Complexity : n+n = 2n = O(n)
+
+// 3) This Method also uses Stack but this time we don't use another double sized array.
+// We make the use of (%) Modulo Operator
 // Total elements 'n' so we check each element 2*n-1 times .
 // As the last element has to be checked again in a rotation one more time.
 vector<int> nextGreaterElements(vector<int> &nums)
@@ -21,12 +29,9 @@ vector<int> nextGreaterElements(vector<int> &nums)
 
     for (int i = 0; i < 2 * n; i++)
     {
-        int currentIdx = i % n;
-        int currentVal = nums[currentIdx];
-
-        while (!st.empty() && nums[st.top()] < currentVal)
+        while (!st.empty() && nums[st.top()] < nums[i % n])
         {
-            res[st.top()] = currentVal;
+            res[st.top()] = nums[i % n];
             st.pop();
         }
 
@@ -39,7 +44,15 @@ vector<int> nextGreaterElements(vector<int> &nums)
     return res;
 }
 
+// 4) Similarly we can apply that other method from size()-1 to 0 as well with same above approach with some things changed 
+
 int main()
 {
+    vector<int> v = {6, 10, 7, 4, 8, 9, 4};
+    v = nextGreaterElements(v);
+    for (int n : v)
+    {
+        cout << n << " ";
+    }
     return 0;
 }
