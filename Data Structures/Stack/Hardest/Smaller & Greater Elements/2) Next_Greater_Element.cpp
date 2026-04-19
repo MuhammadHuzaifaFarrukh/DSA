6f2859135f589d6ac457f5d17b0c851546d04b4e
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2026-04-20 03:37:11 +0500

    Update 2) Next_Greater_Element.cpp

--- Data Structures/Stack/Hardest/Smaller & Greater Elements/2) Next_Greater_Element.cpp
+++ Data Structures/Stack/Hardest/Smaller & Greater Elements/2) Next_Greater_Element.cpp
@@ -8,6 +8,12 @@ using namespace std;
 // For each element in nums1, you must find its position in nums2 and then find the first element to its right in nums2 that is greater than it.
 // The Last ELement will always have -1 as there is no element to its right.
 
+// 1) Brute Force : O(n^2)
+
+// 2) Using Stack : O(n1*n2)
+// Here we find the next greater of nums2 by any of the method we used by stack (2 methods ; from start and from end) , then we take our answer array and compare its positions.
+// So Space Complexity : n+n = 2n = O(n) , we take another vector just for nums2 greater vals.
+// The Last Searching comparison is of n1*n2 so Time Complexity : O(n1*n2).
 vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
 {
     int n2 = nums2.size();
@@ -17,7 +23,7 @@ vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
     // Monotonic Stack to find next greater for all of nums2
     for (int i = 0; i < n2; i++)
     {
-        while (!st.empty() && nums2[st.top()] < nums2[i])
+        while (!st.empty() && nums2[st.top()] <= nums2[i])
         {
             next_vals[st.top()] = nums2[i];
             st.pop();
@@ -25,24 +31,24 @@ vector<int> nextGreaterElement(vector<int> &nums1, vector<int> &nums2)
         st.push(i);
     }
 
-    vector<int> result;
+    vector<int> result(nums1.size(), -1);
     for (int i = 0; i < nums1.size(); i++)
     {
-        int found_val = -1;
-        // Manual search for the element in nums2
+        //  Manual search for the element in nums2
         for (int j = 0; j < n2; j++)
         {
-            if (nums2[j] == nums1[i])
+            if (nums1[i] == nums2[j])
             {
-                found_val = next_vals[j];
+                result[i] = next_vals[j];
                 break;
             }
         }
-        result.push_back(found_val);
     }
     return result;
 }
 
+// 3) Same as above , but finding the next greater of nums2 by using stack from other method as discussed in all other next greater / smaller elements
+// The Last Searching comparison is of n1*n2 so Time Complexity : O(n1*n2).
 int main()
 {
     vector<int> v1 = {4, 1, 2}, v2 = {1, 6, 4, 2};
