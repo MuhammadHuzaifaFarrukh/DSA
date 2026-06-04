2a8cb7ac94baf1b696f3bb6aacf184ddfe1318b8
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2026-06-04 22:47:26 +0500

    Update Valid_Parenthesis.cpp

--- Data Structures/Stack/Parenthesis/Valid_Parenthesis.cpp
+++ Data Structures/Stack/Parenthesis/Valid_Parenthesis.cpp
@@ -4,6 +4,16 @@
 
 using namespace std;
 
+// Brute Forcing may take upto O(n^2).
+
+// By using Stack : 
+// When we find opening brackets , just push it.
+// When we find closing brackets , we need to match it ,and check if stack is empty or not also.
+// If stack is empty it means that closing brackets was the first character or closing brackets was repeated many times.
+// If stack is not empty , it needs to be matched with its correct closing brackets pair. (To avoid this : '({)}' where ) is closed before }  )
+// If it matches , we pop this opening bracket which is at the top of stack.
+// If a mismatch appears , it is not valid.
+// Time : O(n) and Space : O(n)
 bool isValid(string s)
 {
     stack<char> st;
@@ -15,16 +25,13 @@ bool isValid(string s)
         }
         else
         {
-            
             if (st.empty())
             {
                 return false;
             }
 
             char top = st.top();
-            if ((s[i] == ')' && top == '(') ||
-                (s[i] == '}' && top == '{') ||
-                (s[i] == ']' && top == '['))
+            if ((s[i] == ')' && top == '(') || (s[i] == '}' && top == '{') || (s[i] == ']' && top == '['))
             {
                 st.pop(); // Matched!
             }
@@ -34,7 +41,7 @@ bool isValid(string s)
             }
         }
     }
-    return st.empty();		// Stack is Emptied once all chars popped out , and if empty string case given then this gets handled as well.
+    return st.empty(); // Stack is Emptied once all chars popped out , and if empty string case given then this gets handled as well.
 }
 
 int main()
@@ -50,3 +57,36 @@ int main()
     }
     return 0;
 }
+
+// Another (Same) Method but this time storing indices in the stack instead of characters :
+/*
+bool isValid(string s)
+{
+    stack<int> st;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+        {
+            st.push(i);
+        }
+        else
+        {
+            if (st.empty())
+            {
+                return false;
+            }
+
+            char topChar = s[st.top()];
+            if ((s[i] == ')' && topChar == '(') || (s[i] == '}' && topChar == '{') || (s[i] == ']' && topChar == '['))
+            {
+                st.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    return st.empty();
+}
+*/
