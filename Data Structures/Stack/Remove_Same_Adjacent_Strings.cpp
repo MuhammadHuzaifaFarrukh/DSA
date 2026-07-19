d991b07d205041217afe68a223dacc261194512e
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2026-07-19 11:01:31 +0500

    Update Remove_Same_Adjacent_Strings.cpp

--- Data Structures/Stack/Remove_Same_Adjacent_Strings.cpp
+++ Data Structures/Stack/Remove_Same_Adjacent_Strings.cpp
@@ -6,17 +6,24 @@
 using namespace std;
 
 // Remove the adjacent strings that are same and only keep that are not same.
+// Example :s = {"apple", "banana", "banana", "apple", "orange"} , Answer : st.size() = 1 which is {"orange"}
 // Return the stack size
+// This question is same as the Remove All Adjacent Duplicates In String
+
+// Time : O(n)
+// Space : O(n) (stack space if there is no duplicate)
 int remove_adjacent_strings(vector<string> &s)
 {
     stack<string> st;
-    for(int i = 0 ; i<s.size() ; i++)
+    // We can combine the conditions as :
+    // Pop only when !st.empty() && st.top() == s[i] otherwise push
+    for (int i = 0; i < s.size(); i++)
     {
-        if(st.empty())
+        if (st.empty())
         {
             st.push(s[i]);
         }
-        else if(st.top() == s[i])
+        else if (st.top() == s[i]) // !st.empty() && st.top() == s[i]
         {
             st.pop();
         }
@@ -30,5 +37,7 @@ int remove_adjacent_strings(vector<string> &s)
 
 int main()
 {
+    vector<string> v = {"apple", "banana", "banana", "apple", "orange"};
+    cout << " Stack size after removal of same adjacent strings : " << remove_adjacent_strings(v) << endl;
     return 0;
 }
