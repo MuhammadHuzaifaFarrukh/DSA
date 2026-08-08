908d2b9da2fd61b8a5fda21ca6fa8e5ea0c2b1cd
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2026-08-08 15:35:59 +0500

    Update Reverse_Polish_Notation.cpp

--- Data Structures/Stack/Expression Evaluation/Reverse_Polish_Notation.cpp
+++ Data Structures/Stack/Expression Evaluation/Reverse_Polish_Notation.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <vector>
 using namespace std;
 
-int reverse_polish_notation(string tokens[], int n)
+// We are given tokens[] array so multi-digits are handled automatically , no need to parse them here.
+
+int evalRPN(vector<string> &tokens)
 {
-    std::stack<int> s;
+    stack<int> s;
+    int result = 0;
     int op1 = 1;
     int op2 = 1;
-    for (int i = 0; i < n ; i++)
+    for (int i = 0; i < tokens.size(); i++)
     {
         if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/")
         {
@@ -34,9 +38,18 @@ int reverse_polish_notation(string tokens[], int n)
             }
             else if (tokens[i] == "/")
             {
+                if (op2 == 0)
+                {
+                    cout << "Division by Zero , Not Possible , changing Operand to 1" << endl;
+                    op2 = 1;
+                }
                 s.push(op1 / op2);
             }
         }
+    }
+    if (s.empty()) // For Handling empty vector or malformed input vector
+    {
+        return 0;
     }
     return s.top();
 }
@@ -44,22 +57,30 @@ int reverse_polish_notation(string tokens[], int n)
 int main()
 {
     int n;
-    cout << "Enter n : ";
+    cout << "Enter total number of tokens: ";
     cin >> n;
 
-    cin.clear();
-    cin.ignore();
-    string *s = new string[n];
-    cout << "Enter whole expression :" << endl;
+    vector<string> tokens;
+    cout << "\nEnter " << n << " tokens (operands or operators like +, -, *, /):\n";
+
     for (int i = 0; i < n; i++)
     {
-        cout << "Enter a string [" << i << "] : ";
-        getline(cin, s[i]);
-        cout<<endl;
+        string token;
+        cout << "Token [" << i + 1 << "]: ";
+        getline(cin, token);
+        tokens.push_back(token);
     }
-    int x = reverse_polish_notation(s, n);
-    cout << "Answer is : " << x << endl;
 
-    delete[] s;
+    cout << "\nEvaluating RPN Expression: [ ";
+    for (string t : tokens)
+    {
+        cout << t << " ";
+    }
+    cout << "]\n";
+
+    // Compute and display result
+    int result = evalRPN(tokens);
+    cout << "Result: " << result << endl;
+
     return 0;
 }
