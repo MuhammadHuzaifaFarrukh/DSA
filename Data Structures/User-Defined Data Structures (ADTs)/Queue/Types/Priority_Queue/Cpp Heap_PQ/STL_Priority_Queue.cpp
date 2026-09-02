8ec74b5d7ec42f9c4af9ad419a10bc9cbbdc9c67
Muhammad Huzaifa Farrukh <huzaifafarrukh.1990@gmail.com>
2026-09-02 13:15:12 +0500

    Update STL_Priority_Queue.cpp

--- Data Structures/User-Defined Data Structures (ADTs)/Queue/Types/Priority_Queue/Cpp Heap_PQ/STL_Priority_Queue.cpp
+++ Data Structures/User-Defined Data Structures (ADTs)/Queue/Types/Priority_Queue/Cpp Heap_PQ/STL_Priority_Queue.cpp
@@ -7,19 +7,22 @@ using namespace std;
 
 // Priority Queue is actually a heap (either min or max) which is a complete binary tree.
 // In C++ , priority queue is a linear data structure and a derived container.
-// It uses std::vector<T> and also less<T> so it uses vector and max heap by default.
+// It uses std::vector<T> (Adaptee) and also less<T> so it uses vector and max heap by default.
 // We can write greater<int> to make it work like min heap.
 
 // It can even work with those containers that have random access iterators like std::deque , std::array etc.
 // But not with std::list , std::set , std::map etc as they don't have random access iterators or manage their memory by themselve like Red-Black Tree.
 // And also not with std::queue , std::stack cause they are already derived containers and hide their functionality while std::priority_queue needs to see the functions.
 
+// We can make it work for other than built in data types as well :
+// 1) You can also make your own functors of greater<T>() and less<T>() and allow it to work for any data type , any objects , any UDT also.
+// 2) For making it for UDTs , they must have < , > overloaded also so that we can have less<myClass> or greater<myClass>.
 
 int main()
 {
     // --- 1. THE AUTOMATIC WAY (Container Adapter) ---
     // This internally calls make_heap/push_heap for you.
-    priority_queue<int> pq;     // uses vector<int> and make_heap()
+    priority_queue<int> pq; // uses vector<int> and make_heap()
     // Can be used for any specific class as well instead of normal int,float,char etc
     // For that , we need either overload of <,> in our class or functors for < and > in separate structs.
 
@@ -33,7 +36,6 @@ int main()
     cout << "New Top after pop: " << pq.top() << endl;                     // Should be 20
 
     cout << "------------------------------------" << endl;
-
 
     // --- 2. THE MANUAL WAY (The "Under the Hood" Logic) ---
     // This is what the priority_queue class is doing behind the scenes.
